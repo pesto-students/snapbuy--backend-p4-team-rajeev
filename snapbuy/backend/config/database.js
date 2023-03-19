e58b443f5ecef48029a9853243a9d2f6d9cef88c
amitpaswan202 <paswanamit512@gmail.com>
2023-03-19 08:21:47 +0530

    19 march commit

--- snapbuy/backend/config/database.js
+++ snapbuy/backend/config/database.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(con => {
+    mongoose.connect(process.env.DB_URI, 
+       ).then(con =>{
         console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
 }
+
 
 module.exports = connectDatabase
